83ac5ba7f3ba8a569cd99e467217256851e0d79f
Archit Kandu <architkandu@hotmail.com>
2023-01-13 15:57:15 +0545

    Create concate_4.py

---
+++ LAB-1/concate_4.py
@@ -0,0 +1,6 @@
+# Assign 4 string values to variable A,B,C,D. WAP to concatenate the 4 variable with space between 2 subsequent
+A = "First"
+B = "Second"
+C = "Third"
+D = "Fourth"
+print('\nConcatenated String:'," ".join([A,B,C,D]))
