cb0a502ba68dd6856738723b83a54257fb53a882
Archit Kandu <architkandu@hotmail.com>
2023-01-13 16:40:02 +0545

    Create all_arithematic.py

---
+++ LAB-1/all_arithematic.py
@@ -0,0 +1,8 @@
+# WAP to perform all arithematic operation between two float numbers
+f1 = float(input('\nEnter 1st Number: '))
+f2 = float(input('Enter 2nd Number: '))
+print('\nAdd:',"{:.2f}".format(f1 + f2))
+print('Sub:',"{:.2f}".format(f1 - f2))
+print('Mul:',"{:.2f}".format(f1 * f2))
+print('Div:',"{:.2f}".format(f1 / f2))
+print('Mod:',"{:.2f}".format(f1 % f2),'\n')
